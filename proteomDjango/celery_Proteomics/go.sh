9daf70082662ee8b0f13e887f3419987497b8a66
Don9 <dongu7610@naver.com>
2023-05-08 17:41:34 +0900

    commit

--- proteomDjango/celery_Proteomics/go.sh
+++
@@ -1,4 +0,0 @@
-python3 manage.py makemigrations
-python3 manage.py migrate
-systemctl restart proteomeAPI.service
-celery -A celery_prote worker --loglevel=INFO
--- proteomDjango/celery_Proteomics/go_beat.sh
+++
@@ -1,1 +0,0 @@
-celery -A celery_prote beat --loglevel=debug --scheduler django_celery_beat.schedulers:DatabaseScheduler
